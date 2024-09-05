aa02bcc11dcf0eae3b94e721538cf8c978ea36b6
Daisy-Baishya <parintabaishya@gmail.com>
2024-09-05 05:59:48 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const acountId ="1435";
+let accountName="Dev";
+var accountEmail ="web@gmail.com";
+accountCity ="Guwahati"
+
+
+accountName="Daisy"
+accountEmail ="dear@gmail.com"
+accountCity ="mumbai"
+
+console.table([acountId,accountName,accountEmail,accountCity])
